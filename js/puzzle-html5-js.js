e8a3e1c45456329b167548e340ee20c19e4265d4
Tomek <margin-zero@atcplanet.com>
2017-09-21 12:26:38 +0200

    scaling thumbnails and loaded image

--- js/puzzle-html5-js.js
+++ js/puzzle-html5-js.js
@@ -1,22 +1,24 @@
 // global variables
 
-var puzzleArray = ["puzzle001.jpg","puzzle002.jpg"],
+var puzzleArray = ["puzzle001.jpg","puzzle002.jpg","puzzle003.jpg"],
     puzzleDirectory = window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/')) +"/puzzles/";
     puzzlePath = new String;
 
 
-    alert(puzzleDirectory);
-
     $(document).ready(puzzleRun);
 
+
+
+    // Functions =============================================
 
     function puzzleRun() {
         
         var i;
 
-        // add puzzle thumbnails into thumbnail container
+        // add puzzle thumbnails into .puzzleContainer
         for (i=0; i<puzzleArray.length;i++) {
-            $("#puzzleContainer>.puzzles").append("<img src='" + puzzleDirectory + puzzleArray[i] + "' class='puzzleThumbnail' alt='puzzle preview' />");
+            $("#puzzleContainer>.puzzles").append("<div class='puzzleThumbnail'></div>");
+            $("#puzzleContainer>.puzzles>div").eq(i).css("background-size","cover").css("background-image","url('" + puzzleDirectory + puzzleArray[i] +"')");
         };
 
 
@@ -41,7 +43,22 @@ var puzzleArray = ["puzzle001.jpg","puzzle002.jpg"],
             img = new Image;
 
         img.onload = function() {
-            context.drawImage(img, 0, 0);
+            let imgW = img.naturalWidth,
+                imgH = img.naturalHeight,
+                imgOriginX, imgOriginY, imgWidth, imgHeight;
+
+            if ((imgW/imgH)<(4/3)) {
+                imgOriginX = 0;
+                imgOriginY = parseInt(((imgH-(imgW*0.75))/2),10);
+                imgWidth = imgW;
+                imgHeight = parseInt((imgW*0.75),10);
+            } else {
+                imgOriginY = 0;
+                imgOriginX = parseInt(((imgW - (imgH*(4/3))) / 2),10);
+                imgHeight = imgH;
+                imgWidth = parseInt((imgH*(4/3)),10);
+            };
+            context.drawImage(img, imgOriginX, imgOriginY, imgWidth, imgHeight, 0, 0, 800, 600);
         };
 
         puzzlePath = puzzleDirectory + puzzleArray[$this.index()-1];
